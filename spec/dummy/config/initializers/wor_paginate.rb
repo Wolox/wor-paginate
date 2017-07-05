8e100c1d1c1f487e3980ad2baf51f316caa70c63
hugo <hdf1986.6700@gmail.com>
2017-07-05 19:49:09 -0300

    Removed some commented values in initializer

--- spec/dummy/config/initializers/wor_paginate.rb
+++ spec/dummy/config/initializers/wor_paginate.rb
@@ -1,11 +1,9 @@
 Wor::Paginate.configure do |config|
   config.default_per_page = 25
-  # config.default_page = 1
+  config.default_page = 1
 
   config.page_param = :page
   config.per_page_param = :per
 
-  # In case you want to use other format for your response, you can override our formatter here
-  # Check https://url-to-the-docs
-  # config.formatter = Wor::Paginate::Formatter
+  config.formatter = Wor::Paginate::Formatter
 end
