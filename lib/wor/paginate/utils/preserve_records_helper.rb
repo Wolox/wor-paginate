aa94e33f4523fb0edda3a0e5db64bcc22c9d60d0
Martín Mallea <martin.mallea@wolox.com.ar>
2020-02-03 16:40:07 -0300

    Delegate by to PreserveModes2

--- lib/wor/paginate/utils/preserve_records_helper.rb
+++ lib/wor/paginate/utils/preserve_records_helper.rb
@@ -1,4 +1,5 @@
 require_relative 'uri_helper'
+require_relative 'preserve_modes'
 
 module Wor
   module Paginate
@@ -21,38 +22,22 @@ module Wor
 
         def by
           @by ||= begin
-            by = options[:by]&.to_sym || :timestamp
+            by = options[:by]&.to_s || 'timestamp'
             raise ArgumentError, "'by' option shuld be 'id' or 'timestamp'" unless
-              %i[timestamp id].include? by
-
-            by
+              %w[timestamp id].include? by
+            "Wor::Paginate::Utils::PreserveModes::#{by.classify}".constantize
           end
         end
 
         def field
-          @field ||= options[:field] || case by
-                                        when :timestamp
-                                          :created_at
-                                        when :id
-                                          :id
-                                        end
+          @field ||= options[:field] || by.default_field
         end
 
         def last_value
           @last_value ||= begin
             query_param_value = UriHelper.query_params(url)[query_param_name]
-
-            case by
-            when :timestamp
-              query_param_value ? Time.parse(query_param_value) : now_timestamp
-            when :id
-              query_param_value || content.maximum(field)
-            end
+            by.last_value(query_param_value, content, field)
           end
-        end
-
-        def now_timestamp
-          Time.zone.now.iso8601(10)
         end
 
         def query_param_name
